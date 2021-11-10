1ff6d6d8ea47dcccaa6cbae119b2b8d187e7d083
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-10 23:36:15 +0000

    Implement Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.all.count
+    @apparel_count = Product.where(category_id: 1).count
+    @electronic_count = Product.where(category_id: 2).count
+    @furniture_count = Product.where(category_id: 3).count
   end
 end
