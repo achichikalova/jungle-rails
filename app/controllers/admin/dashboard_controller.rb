f4424229d7595c5697fedf4851f7f6c0ba6bc2e2
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-11 16:13:18 +0000

    Implement feature Categories for Admin, fix inconsistency in styles

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+  
   def show
-    @product_count = Product.all.count
-    @apparel_count = Product.where(category_id: 1).count
-    @electronic_count = Product.where(category_id: 2).count
-    @furniture_count = Product.where(category_id: 3).count
+    @category = Category.all
+    @product = Product.all
   end
 end
