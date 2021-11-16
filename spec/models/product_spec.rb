cdd431a01c576f05e9c1edf5a63b48d15f9e7276
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-16 18:34:01 +0000

    Fix specs for Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,34 +11,37 @@ RSpec.describe Product, type: :model do
       :category => category)
   } 
   describe 'Validations' do
-
     it 'should save successfully with all four fields set' do
       expect(subject.name).to be_present
-      expect(subject.price).to be_present
+      expect(subject.price_cents).to be_present
       expect(subject.quantity).to be_present
       expect(subject.category).to be_present
+      subject.valid?
     end
 
     it 'should fail if no name' do
       subject.name = nil
-      expect(subject.errors.full_messages)
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
 
     it 'should fail if no price' do
-      subject.price = nil
-      expect(subject.errors.full_messages)
+      subject.price_cents = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
 
     it 'should fail if no quantity' do
       subject.quantity = nil
-      expect(subject.errors.full_messages)
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
 
     it 'should fail if no category' do
       subject.category = nil
-      expect(subject.errors.full_messages)
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
-    
   end
   
 end
